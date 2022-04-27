e1e6ca6bde5230b6194b10d05f4f3a4a0565b33e
clee85 <53160124+clee85@users.noreply.github.com>
2022-04-27 11:05:50 +0800

    Update s3_bucket.tf

--- aws/s3_bucket.tf
+++ aws/s3_bucket.tf
@@ -1,30 +1,8 @@
-resource "aws_s3_bucket" "tenable_cs_demo_s3_bucket" {
-  bucket = "tenablecsdemos3bucket"
-  acl    = "public-read"
-  tags   = var.default_tags
+resource "aws_s3_bucket" "tcs-demo-s3" {
+  bucket = "tcs-demo-s3-bucket"
 }
-resource "aws_s3_bucket_policy" "tenable_cs_demo_s3_bucketpolicy" {
-  bucket = aws_s3_bucket.tenable_cs_demo_s3_bucket.id
 
-  policy = <<POLICY
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Sid": "tenable_cs_demo_s3_bucket-restrict-access-to-users-or-roles",
-      "Effect": "Allow",
-      "Principal": [
-        {
-          "AWS": [
-            "arn:aws:iam::##acount_id##:role/##role_name##",
-            "arn:aws:iam::##acount_id##:user/##user_name##"
-          ]
-        }
-      ],
-      "Action": "s3:GetObject",
-      "Resource": "arn:aws:s3:::tenable_cs_demo_s3_bucket/*"
-    }
-  ]
-}
-POLICY
+resource "aws_s3_bucket_acl" "tcs-demo-s3-acl" {
+  bucket = aws_s3_bucket.tcs-demo-s3.id
+  acl    = "public-read"
 }
